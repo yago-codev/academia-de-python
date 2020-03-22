b3425fb6350d7f23bb4c5e880d6b341bb5fd08d6
yago-codev <yagomilano92@gmail.com>
2020-03-22 15:43:04 -0300

    operacões matemáticas

---
+++ modulo02/operacoes_matematicas.py
@@ -0,0 +1,23 @@
+# adição
+soma = 1 + 1
+print(soma)
+
+# subtracão
+subtracao = 2 - 1
+print(subtracao)
+
+# multiplicação
+multiplicacao = 2 * 2
+print(multiplicacao)
+
+# divisao
+divisao = 6 / 2
+print(divisao)
+
+# divisao: retornando somente a parte inteira
+divisao_parte_inteira =  5 // 2
+print(divisao_parte_inteira)
+
+# resto da divisao
+resto_da_divisao = 17 % 6
+print(resto_da_divisao)
