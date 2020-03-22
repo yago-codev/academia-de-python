b9cf645b2254a9be3300ee6647ffd734b22a39ca
yago-codev <yagomilano92@gmail.com>
2020-03-22 15:54:09 -0300

    conhecendo a função input()

---
+++ modulo02/entrada.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*- # instrução para especificar o padrão de caracteres com acentuação
+
+# pedir o nome do usuário
+nome = input("Digite o seu nome: ") # a função input funciona com o terminal e serve para armazenar uma string informada pelo usuário
+
+# mostrar na tela: "Olá, (nome_do_usuario)"
+print(f"Olá, {nome}")
