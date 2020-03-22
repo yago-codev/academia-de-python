e8d8ee83dfe0919d82db544c1b2fa1631ac0643b
yago-codev <yagomilano92@gmail.com>
2020-03-22 16:05:19 -0300

    tipos primitivos: int, float, str

---
+++ modulo02/tipos_de_dados.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# inteiro -> int
+tipo_inteiro = 42
+print(type(tipo_inteiro))
+
+# ponto flutuante (numeros com vírgula) -> float
+tipo_float = 42.0
+print(type(tipo_float))
+
+# palavras/letras -> string -> str
+tipo_string = "42"
+print(type(tipo_string))
